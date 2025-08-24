ddfb2a4a4100b36190af42ee7bd8a84f05bc8111
Deepak <dkbainsla8@gmail.com>
2025-08-24 23:31:31 +0530

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import Work from './components/Work/Work'
 import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
-import Navbar from './components/navbar/Navbar.JSX'
+import Navbar from './components/navbar/Navbar.jsx'
 import Couple from './components/Couple/Couple'
 
 
