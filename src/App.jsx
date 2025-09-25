724d17eb18324ee33114f4764e092d087b46dfcc
Deepak <dkbainsla8@gmail.com>
2025-09-25 11:27:04 +0530

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./index.css";
 
@@ -10,22 +9,6 @@ import Couple from "./components/Couple/Couple";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-=======
-import React from 'react'
-import './index.css'
-
-
-import Work from './components/Work/Work'
-import Contact from './components/Contact/Contact'
-import Home from './components/Home/Home'
-import Navbar from './components/navbar/Navbar'
-import Couple from './components/Couple/Couple'
-
-
-function App() {
-
-
->>>>>>> ae6bf044c5390aabd04ba5193a50c66ab9f46b6d
   return (
     <>
       <Navbar />
@@ -33,19 +16,9 @@ function App() {
       <Work />
       <Couple />
       <Contact />
-<<<<<<< HEAD
       <Footer />
     </>
   );
 }
 
 export default App;
-=======
-
-
-    </>
-  )
-}
-
-export default App
->>>>>>> ae6bf044c5390aabd04ba5193a50c66ab9f46b6d
